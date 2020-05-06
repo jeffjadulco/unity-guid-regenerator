ed63ede19b784a144a484b6ec2157b1423ffaf81
Jeff Jadulco <jeffjadulco.work@gmail.com>
2020-05-06 21:20:30 +0800

    Add ScriptableObject demo

---
+++ unity-guid-generator/Assets/Jads/Demo/GUIDRegeneratorDemoScriptableObject.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+namespace Jads.Demo
+{
+    [CreateAssetMenu(menuName = "Assets/GUID Regenerator Demo/ScriptableObject", fileName = "GUIDRegeneratorDemoScriptableObject")]
+    public class GUIDRegeneratorDemoScriptableObject : ScriptableObject
+    {
+        [SerializeField] [Range(0, 100)] private int demoInt;
+        [SerializeField] private string demoString;
+        [SerializeField] private Material demoMaterial;
+        [SerializeField] private Sprite demoSprite;
+        [SerializeField] private GameObject demoPrefab;
+        [SerializeField] private AudioClip demoAudio;
+    }
+}
